4640e40134ba17f5c7b5f320f7f0449efbfb1f95
HwaEowm <thkim025@gmail.com>
2026-08-08 01:21:26 +0900

    Update full_adder.v

--- rtl/fulll_adder/full_adder.v
+++ rtl/fulll_adder/full_adder.v
@@ -14,16 +14,14 @@ endmodule
 // adder_ripple
     
 module adder_ripple(a, b, q);
-        
-        input [3:0] a, b;
-        output [3:0] q;
-        wire [3:0] co;
-        
-        // making instances
-        // 1'b0 : means binary 1 bit '0'
-        fulladd add0 (a[0], b[0], 1'b0, q[0], co[0]);
-        fulladd add1 (a[1], b[1], co[0], q[1], co[1]);
-        fulladd add2 (a[2], b[2], co[1], q[2], co[2]);
-        fulladd add3 (a[3], b[3], co[2], q[3], co[3]);
-        
+    input [3:0] a, b;
+    output [3:0] q;
+    wire [3:0] co;
+
+    // making instances
+    // 1'b0 : means binary 1 bit '0'
+    fulladd add0 (.a(a[0]), .b(b[0]), .c_in(1'b0),  .q(q[0]), .c_out(co[0]));
+    fulladd add1 (.a(a[1]), .b(b[1]), .c_in(co[0]), .q(q[1]), .c_out(co[1]));
+    fulladd add2 (.a(a[2]), .b(b[2]), .c_in(co[1]), .q(q[2]), .c_out(co[2]));
+    fulladd add3 (.a(a[3]), .b(b[3]), .c_in(co[2]), .q(q[3]), .c_out(co[3])); 
 endmodule
